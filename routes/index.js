18e7bc0acca83bae6285fe271bc4edbfb03eb74e
MiloslavStekrt <stekrt18@gmail.com>
2020-12-20 16:24:54 +0100

    index controller

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'BEAM-ms' });
+});
+
+router.get('/school', (req, res) => {
+  res.render('school', {title: "BEAM - my schoool"})
+})
+
+module.exports = router;
