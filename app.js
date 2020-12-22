194b5db128a66c18b2057e42b660c30daa40d79e
MiloslavStekrt <stekrt18@gmail.com>
2020-12-22 15:10:23 +0100

    commit before

--- app.js
+++ app.js
@@ -3,15 +3,36 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mysql = require('mysql');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
+// include controllers form routes
 const indexRouter = require('./routes/index');
 const studentRouter = require('./routes/student');
 
 const app = express();
 
-// view engine setup
+// setuping
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+const connection = mysql.createConnection({
+	host     : process.env.DB_HOST,
+	user     : process.env.DB_USER,
+	password : process.env.DB_PS,
+	database : process.env.DB_NAME
+});
+
+// middleware
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,16 +40,42 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes shower
 app.use('/', indexRouter);
 app.use('/student', studentRouter);
+app.post("/auth", (req, res) => {
+  if(req.session.loggedin){
+    res.redirect("/school");
+    return;
+  }
+  let username = req.body.uid;
+  let password = req.body.pwd;
+  if (username && password) {
+    connection.query(
+      'SELECT * FROM users WHERE email = ? AND pwd = ?',
+      [username, password], (error, result, fields) => {
+      if(result.length > 0){
+        req.session.loggedin = true;
+        req.session.usernam = username;
+        console.log(result);
+        res.redirect("/");
+      }else{
+        res.send("incorrect username of password");
+      }
+      return;
+    });
+  }else {
+    res.send("Password or username is empty");
+  }
+  return;
+})
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const session = require('express-session');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'BEAM-ms' });
-});
+  let link;
+  let isLogged = false;
+  if (req.session.loggedin) {
+		link = "My School";
+    isLogged = true;
+	} else {
+		link = "Join us";
+	}
+  res.render('index', {
+    title: 'BEAM-ms',
+    link: link,
+    loggedin: isLogged
+  })
+})
 
 router.get('/school', (req, res) => {
-  res.render('school', {title: "BEAM - my schoool"})
+  if(req.session.loggedin){
+    res.render('school', {
+      title: "BEAM - my schoool",
+      loggedin: true
+    })
+  }else{
+    res.redirect("/");
+  }
 })
+
+router.get('/login', (req, res) => {
+  if(!req.session.loggedin){
+    const navlinks = navbar(false);
+    res.render('login', {
+      title: "Login to BEAM",
+      loggedin: false
+    })
+  }else{
+    res.redirect("/school")
+  }
+})
+
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+})
+
+// router.get('/logout', ())
 
 module.exports = router;
