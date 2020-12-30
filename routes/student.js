82cf931f033e08fee5c55e16545e7c664a134679
MiloslavStekrt <stekrt18@gmail.com>
2020-12-30 17:49:46 +0100

    repire sending lessons_time

--- routes/student.js
+++ routes/student.js
@@ -16,60 +16,67 @@ router.get('/', (req, res) => {
   res.redirect("/school");
 });
 
-router.get('/student', (req, res) => {
-  res.redirect("/student/school");
+router.get('/school', (req, res) => {
+	if(!req.session.loggedin){
+    res.redirect("/login");
+  }else{
+		let role = req.session.role;
+		let id = req.session.id;
+
+		if(req.session.role != 1){
+			console.log("You are not student")
+			res.redirect("/teacher");
+		}else{
+			let classes = [];
+			let lessons_time = [];
+
+			connection.query(
+				'SELECT * FROM `class_student` WHERE `id_user`=?',
+				[id], (error, result, fields) => {
+					if(!result){
+						result.forEach(item => {
+							connection.query(
+								'SELECT * FROM `classes` WHERE `id`=?',
+								[item.id_class], (error, result, fields) => {
+									classes.push(result[0]);
+								}
+							);
+						});
+					}
+					classes.forEach( clas => {
+						let time = item.time;
+						if(time.includes('.')){
+							time = time.replace(' ', '').split('.');
+						}else{
+							time = [time];
+						}
+						time.forEach(item => {
+							let timer = time.split('-');
+							lessons_time.push({
+								id_class: clas.id,
+								name: clas.name,
+								day: timer[0].toLowerCase(),
+								time: Number(timer[1])
+							});
+						});
+					});
+					connection.end();
+					res.render('school', {
+						role: role,
+						title: "BMS - My school",
+						lessons_time: lessons_time,
+					});
+				}
+			);
+		}
+	}
 });
 
-router.get('/school', (req, res) => {
-  let role = req.session.role;
-  let id = req.session.id;
-  let classes = [];
-  let lessons_time = []
-
-  if(role >= 2){
-    res.redirect("/teacher/school");
-  }
-
-  connection.query(
-    'SELECT * FROM `class_student` WHERE `id_user`=?',
-    [id], (error, result, fields) => {
-      result.forEach((item) => {
-        connection.query(
-          'SELECT * FROM `classes` WHERE `id`=?',
-          [item.id_class], (error, result, fields) => {
-            classes = result;
-          }
-        )
-      });
-      classes.forEach( clas => {
-        let time = item.time;
-        if(time.includes('.')){
-          time = time.replace(' ', '').split('.');
-        }else{
-          time = [time];
-        }
-        time.forEach(item => {
-          let timer = time.split('-');
-          lessons_time.push({
-            id_class: clas.id,
-            name: clas.name,
-            day: timer[0].toLowerCase(),
-            time: Number(timer[1])
-          });
-        });
-      });
-      connection.end();
-    }
-  )
-  res.render('school', {
-    role: role,
-    title: "BMS - My school",
-    lessons_time: lessons_time,
-  })
-})
-
 router.get('/marks', (req, res) => {
-  if(req.session.role){
+	if(!req.session.loggedin){
+    res.redirect("/login");
+  }
+  if(Number(req.session.role)  === 1){
     res.render('mark', {
       title: "BEAM - my schoool",
       role: req.session.role
