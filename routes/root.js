5f8c715fed3ba62735e3f8742d7f4044b7a02a13
MiloslavStekrt <stekrt18@gmail.com>
2021-01-04 15:37:53 +0100

    change messange to msg to work

--- routes/root.js
+++ routes/root.js
@@ -259,12 +259,12 @@ router.post('/edituser/:id', (req, res)=>{
 })
 
 router.post('/messenge', (req, res)=>{
-	let msg = req.body.messange;
+	let msg = req.body.msg;
 	let id = req.session.user_id;
 	connection.query("INSERT INTO `msg`(`id_user`, `content`) VALUES (?, ?)",[id, msg],(error, result, fields)=>{
 		if(error){console.log(error);}
-		return;
-	})
+		res.redirect("/root")
+	});
 })
 
 // redirect to index controller
