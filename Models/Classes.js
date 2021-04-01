d5eeb87e08700679684baae3fed5d0f1ff7ce3b8
MiloslavStekrt <stekrt18@gmail.com>
2021-04-01 19:32:39 +0200

    better class model

--- Models/Classes.js
+++ Models/Classes.js
@@ -2,26 +2,10 @@ const mongoose = require('mongoose');
 // let code_raw = Math.random().toString(16).substr(2, 8)
 const Class = mongoose.Schema(
     {
-        name: {
-            type: String,
-            required: true
-        },
-        code: {
-            type: String,
-            require: true
-        },
-        teacher: {
-            type: Object,
-            required: true
-        },
-        students: {
-            type: Array,
-            required: false
-        },
-        avg: {
-            type: Number,
-            required: false
-        }
+        name: String,
+        teacher: Object,
+        students: Array,
+        avg: Number
     }
 )
 
